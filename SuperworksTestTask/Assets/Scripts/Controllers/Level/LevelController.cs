d15c70e58d57d91060b66ff22034dc1d2ee3b96e
valerii.zakharov <v.zakharov@program-ace.com.ua>
2024-03-18 13:16:14 +0100

    Decrease complete popup show delay.

--- SuperworksTestTask/Assets/Scripts/Controllers/Level/LevelController.cs
+++ SuperworksTestTask/Assets/Scripts/Controllers/Level/LevelController.cs
@@ -87,7 +87,7 @@ namespace ZiplineValley.Controllers
         private IEnumerator CoroutineShowCompletePopupAfterParticles()
         {
             _levelCompletedParticles.SetActive(true);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1.5f);
             _userInterface.EndGamePopupView.Show(
                 true,
                 _levelModel.AliveCharacters, 
